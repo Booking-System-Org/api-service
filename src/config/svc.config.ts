7170ec62a9647b69f52adc625778cc312caee688
Enestacy <103126316+Enestacy@users.noreply.github.com>
2025-09-10 15:11:46 +0300

    chore: refactor & clean up

--- src/config/svc.config.ts
+++ src/config/svc.config.ts
@@ -5,6 +5,6 @@ export default registerAs(
   'service',
   (): AppConfig => ({
     appPort: process.env.APP_PORT || '3000',
-    appEnv: process.env.NODE_ENV || 'development',
+    appEnv: `${process.env.NODE_ENV}`,
   }),
 );
